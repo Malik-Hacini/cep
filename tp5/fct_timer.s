a32623c62e5a4ee27c00b3b0b07281119cf25159
hacinim <malik.hacini@grenoble-inp.fr>
2025-04-27 18:17:40 +0200

    add timer

--- tp5/fct_timer.s
+++ tp5/fct_timer.s
@@ -3,15 +3,32 @@
     .globl reveil
 /* void reveil(uint32_t delta_t); */
 reveil:
-/* A completer */
-    ret
+    /* a0 = delta_t --------------------------------------------------- */
+
+    /* 1. Lire l’horloge courante (mtime) 64 bits --------------------- */
+    la      t1, CLINT_TIMER        /* Base du registre mtime          */
+    lw      t2, 0(t1)              /* t2 = mtime_lo                   */
+    lw      t3, 4(t1)              /* t3 = mtime_hi                   */
+
+    /* 2. heure_future = mtime + delta_t -------------------- */
+    add     t4, t2, a0             /* t4 = future_lo                  */
+    sltu    t5, t4, t2             /* t5 = 1 si débordement (carry)   */
+    add     t3, t3, t5             /* future_hi = old_hi + carry      */
+
+    /* 3. Programmer mtimecmp  -------------------------------------- */
+    li      t0, -1                 /* 0xFFFF_FFFF                     */
+    la      t1, CLINT_TIMER_CMP    /* Base du registre mtimecmp       */
+    sw      t0, 0(t1)              /* 1.  Bloquer la partie basse*/
+    sw      t3, 4(t1)              /* 2. Ecrire la partie haute */
+    sw      t4, 0(t1)              /* 3. Ecrire la partie basse */
+    ret                            
 
     .globl gestion_interruptions
 gestion_interruptions:
-    addi sp, sp, -4 # Juste ra à sauver
+    addi sp, sp, -4 
     sw   ra, 0(sp)
     andi t0, a0, 0xf
-    li   t1, IRQ_M_TMR  # Interruption d'horloge
+    li   t1, IRQ_M_TMR  # Interruption horloge
     bne  t0, t1, interruption_externe
     jal  mon_vecteur_horloge
     j    retour
